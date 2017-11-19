e9bbbfef9c9b70f1680c127888412ce61c98b36b
acetechnology <amaechiemeka.o@gmail.com>
2017-11-19 10:21:10 +0100

    first

--- app/src/main/java/com/arcadiamobiletv/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/arcadiamobiletv/swoosh/WelcomeActivity.kt
@@ -2,11 +2,13 @@ package com.arcadiamobiletv.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+       // window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
